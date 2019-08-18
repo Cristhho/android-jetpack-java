ebdd09204cd9b3b77600756e648c19a51206c0a9
cristhian ochoa <cristhho@gmail.com>
2019-08-18 14:14:18 -0500

    fragments navigation

--- app/src/main/java/com/cristhian/dogsapp/view/ListFragment.java
+++ app/src/main/java/com/cristhian/dogsapp/view/ListFragment.java
@@ -4,16 +4,24 @@ package com.cristhian.dogsapp.view;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavDirections;
+import androidx.navigation.Navigation;
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.cristhian.dogsapp.R;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class ListFragment extends Fragment {
 
+    @BindView(R.id.floatingActionButton)
+    FloatingActionButton fab;
 
     public ListFragment() {
     }
@@ -22,7 +30,23 @@ public class ListFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_list, container, false);
+
+        View view = inflater.inflate(R.layout.fragment_list, container, false);
+        ButterKnife.bind(this, view);
+
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        fab.setOnClickListener(v -> goToDetails());
+    }
+
+    private void goToDetails() {
+        NavDirections action = ListFragmentDirections.actionDetail();
+        Navigation.findNavController(fab).navigate(action);
     }
 
 }
--- app/src/main/java/com/cristhian/dogsapp/view/MainActivity.java
+++ app/src/main/java/com/cristhian/dogsapp/view/MainActivity.java
@@ -1,6 +1,10 @@
 package com.cristhian.dogsapp.view;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
 
@@ -8,9 +12,19 @@ import com.cristhian.dogsapp.R;
 
 public class MainActivity extends AppCompatActivity {
 
+    private NavController mNavController;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mNavController = Navigation.findNavController(this, R.id.fragment);
+        NavigationUI.setupActionBarWithNavController(this, mNavController);
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        return NavigationUI.navigateUp(mNavController, (DrawerLayout) null);
     }
 }
