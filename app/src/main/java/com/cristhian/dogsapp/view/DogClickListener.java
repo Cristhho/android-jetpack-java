bf6b3a452851e0bc83f76aeb3ae8297594c520ea
cristhian ochoa <cristhho@gmail.com>
2019-08-19 23:36:11 -0500

    binding methods call

---
+++ app/src/main/java/com/cristhian/dogsapp/view/DogClickListener.java
@@ -0,0 +1,9 @@
+package com.cristhian.dogsapp.view;
+
+import android.view.View;
+
+public interface DogClickListener {
+
+    public void onDogClicked(View v);
+
+}
--- app/src/main/java/com/cristhian/dogsapp/view/DogsListAdapter.java
+++ app/src/main/java/com/cristhian/dogsapp/view/DogsListAdapter.java
@@ -3,14 +3,11 @@ package com.cristhian.dogsapp.view;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
-import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.cristhian.dogsapp.R;
 import com.cristhian.dogsapp.databinding.ItemDogBinding;
 import com.cristhian.dogsapp.model.DogBreed;
-import com.cristhian.dogsapp.util.Util;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -20,7 +17,8 @@ import androidx.databinding.DataBindingUtil;
 import androidx.navigation.Navigation;
 import androidx.recyclerview.widget.RecyclerView;
 
-public class DogsListAdapter extends RecyclerView.Adapter<DogsListAdapter.DogViewHolder> {
+public class DogsListAdapter extends RecyclerView.Adapter<DogsListAdapter.DogViewHolder>
+        implements DogClickListener{
 
     private List<DogBreed> dogsList;
 
@@ -45,24 +43,16 @@ public class DogsListAdapter extends RecyclerView.Adapter<DogsListAdapter.DogVie
     @Override
     public void onBindViewHolder(@NonNull DogViewHolder holder, int position) {
         holder.itemView.setDogBreed(dogsList.get(position));
-//        ImageView image = holder.itemView.findViewById(R.id.imageView);
-//
-//        TextView name = holder.itemView.findViewById(R.id.dogName);
-//        TextView dogLifespan = holder.itemView.findViewById(R.id.dogLifespan);
-//
-//        LinearLayout layout = holder.itemView.findViewById(R.id.dogLayout);
-//
-//        name.setText(dogsList.get(position).dogBreed);
-//        dogLifespan.setText(dogsList.get(position).lifeSpan);
-//
-//        Util.loadImage(image, dogsList.get(position).imageUrl, Util.getProgressDrawable(
-//                image.getContext()));
-//
-//        layout.setOnClickListener(v -> {
-//            ListFragmentDirections.ActionDetail action = ListFragmentDirections.actionDetail();
-//            action.setDogUuid(dogsList.get(position).uuid);
-//            Navigation.findNavController(layout).navigate(action);
-//        });
+        holder.itemView.setListener(this);
+    }
+
+    @Override
+    public void onDogClicked(View v) {
+        String uuidString = ((TextView)v.findViewById(R.id.dogId)).getText().toString();
+        int uuid = Integer.valueOf(uuidString);
+        ListFragmentDirections.ActionDetail action = ListFragmentDirections.actionDetail();
+        action.setDogUuid(uuid);
+        Navigation.findNavController(v).navigate(action);
     }
 
     @Override
